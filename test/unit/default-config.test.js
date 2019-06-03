d50ebc454dcfec582707ba78959110ca4c5e6f2a
Jonathan Alexander <me@jmalexan.com>
2019-06-02 22:26:25 -0700

    some bug fixes on the unit tests

--- test/unit/default-config.test.js
+++ test/unit/default-config.test.js
@@ -5,57 +5,55 @@ import toElectronBackgroundColor from '../../app/utils/to-electron-background-co
 const dc = require('../../app/config/config-default.js');
 
 test('When checking the default config', t => {
+  t.true(dc.config.updateChannel === 'stable' || dc.config.updateChannel === 'canary');
 
-    t.true(dc.config.updateChannel === 'stable' || dc.config.updateChannel === 'canary');
+  t.true(typeof dc.config.fontSize === 'number');
 
-    t.true(typeof dc.config.fontSize === 'number');
+  t.true(dc.config.fontWeightBold === 'normal' || dc.config.fontWeightBold === 'bold');
 
-    t.true(dc.config.fontWeightBold === 'normal' || dc.config.fontWeightBold === 'bold');
+  t.true(dc.config.fontWeight === 'normal' || dc.config.fontWeightBold === 'bold');
 
-    t.true(dc.config.fontWeight === 'normal' || dc.config.fontWeightBold === 'bold');
+  t.true(typeof dc.config.lineHeight === 'number');
 
-    t.true(typeof dc.config.lineHeight === 'number');
+  t.true(typeof dc.config.letterSpacing === 'number');
 
-    t.true(typeof dc.config.letterSpacing === 'number');
+  t.true(isHexColor(toElectronBackgroundColor(dc.config.cursorColor)));
 
-    t.true(isHexColor(toElectronBackgroundColor(dc.config.cursorColor)));
+  t.true(isHexColor(toElectronBackgroundColor(dc.config.cursorAccentColor)));
 
-    t.true(isHexColor(toElectronBackgroundColor(dc.config.cursorAccentColor)));
+  t.true(
+    dc.config.cursorShape === 'BEAM' || dc.config.cursorShape === 'UNDERLINE' || dc.config.cursorShape === 'BLOCK'
+  );
 
-    t.true( dc.config.cursorShape === 'BEAM'
-    ||      dc.config.cursorShape === 'UNDERLINE'
-    ||      dc.config.cursorShape === 'BLOCK'
-    );
+  t.true(typeof dc.config.cursorBlink === 'boolean');
 
-    t.true(typeof dc.config.cursorBlink === 'boolean');
+  t.true(isHexColor(toElectronBackgroundColor(dc.config.foregroundColor)));
 
-    t.true(isHexColor(toElectronBackgroundColor(dc.config.foregroundColor)));
+  t.true(isHexColor(toElectronBackgroundColor(dc.config.backgroundColor)));
 
-    t.true(isHexColor(toElectronBackgroundColor(dc.config.backgroundColor)));
+  t.true(isHexColor(toElectronBackgroundColor(dc.config.selectionColor)));
 
-    t.true(isHexColor(toElectronBackgroundColor(dc.config.selectionColor)));
+  t.true(isHexColor(toElectronBackgroundColor(dc.config.borderColor)));
 
-    t.true(isHexColor(toElectronBackgroundColor(dc.config.borderColor)));
+  t.true(typeof dc.config.padding === 'string');
 
-    t.true(typeof dc.config.padding === 'string');
+  t.true(typeof dc.config.shell === 'string');
 
-    t.true(typeof dc.config.shell === 'string');
+  t.true(dc.config.bell === 'SOUND' || dc.config.bell === false);
 
-    t.true(dc.config.bell === 'SOUND' || dc.config.bell === false);
+  t.true(typeof dc.config.copyOnSelect === 'boolean');
 
-    t.true(typeof dc.config.copyOnSelect === 'boolean');
+  t.true(typeof dc.config.defaultSSHApp === 'boolean');
 
-    t.true(typeof dc.config.defaultSSHApp === 'boolean');
+  t.true(typeof dc.config.quickEdit === 'boolean');
 
-    t.true(typeof dc.config.quickEdit === 'boolean');
+  t.true(dc.config.macOptionSelectionMode === 'vertical' || dc.config.macOptionSelectionMode === 'force');
 
-    t.true(dc.config.copyOnSelect === 'vertical' || dc.config.copyOnSelect === 'force');
+  t.true(typeof dc.config.webGLRenderer === 'boolean');
 
-    t.true(typeof dc.config.webGLRenderer === 'boolean');
+  t.true(dc.plugins instanceof Array);
 
-    t.true(dc.plugins.isArray());
+  t.true(dc.localPlugins instanceof Array);
 
-    t.true(dc.localPlugins.isArray());
-
-    t.true(typeof dc.keymaps === 'object');
+  t.true(typeof dc.keymaps === 'object');
 });
--- test/unit/windows-config.test.js
+++ test/unit/windows-config.test.js
@@ -1,25 +1,20 @@
 import test from 'ava';
-import {get, recordState} from '../../app/config/windows.js';
-import Window from '../../app/ui/window.js'
-
+import {defaults} from '../../app/config/windows.js';
 
 test('when checking "app/config/windows.js"', t => {
+  t.true(defaults.windowPosition.length === 2);
 
-        t.true(
-            get().position.length === 2
-        );
+  t.true(defaults.windowSize.length === 2);
 
-        t.true(
-            get().size.length === 2
-        );
+  t.true(
+    defaults.windowPosition.reduce((tt, v) => {
+      typeof v === 'number' && tt;
+    }, true)
+  );
 
-        t.true(
-            get().size.reduce( (t, v) => {typeof v === 'number' && t }, true)
-        );
-
-        t.true(
-            get().position.reduce( (t, v) => {typeof v === 'number' && t }, true)
-        );
-
-    }
-);
+  t.true(
+    defaults.windowSize.reduce((tt, v) => {
+      typeof v === 'number' && tt;
+    }, true)
+  );
+});
